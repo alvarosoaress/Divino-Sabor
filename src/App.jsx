cb5f7a92c4fc8d62363ee25e96f3b557ffdb497f
alvarosoaress <alvarosoares01@gmail.com>
2023-03-29 19:59:25 -0300

    path fix #3

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ export default function App() {
   return (
     <ThemeProvider theme={colors}>
       <BrowserRouter>
-        <GlobalStyles />
         <Router />
+        <GlobalStyles />
       </BrowserRouter>
     </ThemeProvider>
   );
